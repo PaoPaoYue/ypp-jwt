a9353897360ddf9a0cdfc5ae9a444f1adca99cb1
PaoPaoYue <846260131@qq.com>
2020-10-06 16:38:56 +0800

    Spring initializer files added

---
+++ src/main/java/com/ypp/yppjwt/YppJwtApplication.java
@@ -0,0 +1,13 @@
+package com.ypp.yppjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YppJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YppJwtApplication.class, args);
+	}
+
+}
